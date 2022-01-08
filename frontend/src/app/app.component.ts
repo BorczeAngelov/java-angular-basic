97d8267faf648daa3e8c3c89393f08b89229022b
Borcze Angelov <Borcze.Angelov@yahoo.com>
2022-01-08 19:15:54 +0100

    display response from pingServer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {first, map, tap} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { first, map, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +9,29 @@ import {first, map, tap} from 'rxjs/operators';
   styleUrls: ['./app.component.css'],
   providers: [HttpClient]
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'frontend';
   message = '';
+  pingMessage = '';
+  hostNameUrl: string;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('http://localhost:8080/message').pipe(
+
+    this.hostNameUrl = "//" + window.location.hostname;
+
+    this.http.get(this.hostNameUrl + '/message').pipe(
       first(),
       tap(result => console.log('Message received from the server: ', result)),
       map(result => this.message = (result as any).message)
     ).subscribe();
+
+    this.http.get(this.hostNameUrl + '/api/pingServer').pipe(
+      first(),
+      tap(result => console.log('pingMessage received from the server: ', result)),
+      map(result => this.pingMessage = (result as any).message)
+    ).subscribe();    
   }
 
 }
